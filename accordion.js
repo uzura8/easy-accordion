89b37e9c38500728b7f37d6599141bdf958b6d33
Shingo Yamada <uzuranoie@gmail.com>
2019-01-06 00:32:58 +0900

    First commit

---
+++ accordion.js
@@ -0,0 +1,48 @@
+'use strict';
+
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('js-accordion') === false) return;
+
+  var $el = event.target;
+  var activeClass = $el.dataset.active_class !== undefined ? $el.dataset.active_class : 'is-active';
+  var isScroll = $el.dataset.scroll == 1 ? $el.dataset.scroll : false;
+  var groupSelector = $el.dataset.group !== undefined ? $el.dataset.group : '';
+  var target = $el.dataset.target;
+  var $target = target !== undefined ? document.getElementById(target) : $el.nextElementSibling;
+  var toOpen = !$target.classList.contains(activeClass);
+
+  // Close other accordions
+  var $groupParent = null;
+  if (toOpen && groupSelector.length > 0) {
+    $groupParent = (function(elem, selector) {
+      for (; elem && elem !== document; elem = elem.parentNode) {
+        if (elem.matches(selector)) return elem;
+      }
+      return null;
+    })($el, groupSelector);
+    if ($groupParent !== null) {
+      for (var i = 0, n = $groupParent.children.length; i < n; i++) {
+        var $child = $groupParent.children[i];
+        var $childTrigger = $child.querySelector('.js-accordion');
+        if ($childTrigger) $childTrigger.classList.remove(activeClass);
+        var $childAccordion = $child.querySelector('.accordion-content');
+        if ($childAccordion) $childAccordion.classList.remove(activeClass);
+      }
+    }
+  }
+
+  // Toggle target accordion
+  $el.classList.toggle(activeClass);
+  if ($target !== null) $target.classList.toggle(activeClass);
+
+  // Scroll
+  if (toOpen && isScroll) {
+    var $scrollTarget = $groupParent ? $groupParent : $el;
+    (function(targetElm) {
+      var targetPosY = targetElm.getBoundingClientRect().top;
+      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      var targetTop = targetPosY + scrollTop;
+      window.scrollTo(0, targetTop);
+    })($scrollTarget);
+  }
+}, false);
